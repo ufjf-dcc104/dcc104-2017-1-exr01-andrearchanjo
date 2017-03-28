bff497e88af73b53e90ac06001851ef8f894057a
André Archanjo <archanjoandre@yahoo.com.br>
2017-03-28 18:52:20 -0300

    Adicionar pouso

--- Sprite.js
+++ Sprite.js
@@ -1,14 +1,14 @@
 function Sprite(){
-  this.x = 0;
+  this.x = 192.5;
   this.y = 0;
   this.vx = 0;
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
   this.color = "blue";
-}
+};
 
-Sprite.prototype.desenhar = function (ctx) {
+Sprite.prototype.desenharQuadrado = function (ctx) {
   ctx.fillStyle = this.color;
   ctx.fillRect(this.x, this.y, 15, 15);
   ctx.strokeStyle = "black";
@@ -20,7 +20,53 @@ Sprite.prototype.mover = function (dt) {
   this.vy = this.vy + (this.ay+60)*dt;
   this.x = this.x + this.vx*dt;
   this.y = this.y + this.vy*dt;
-
-
-
 };
+
+Sprite.prototype.desenharRetangulo = function (ctx) {
+  ctx.fillStyle = "gray";
+  ctx.fillRect(this.x, this.y, 400, 100);
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 425, 400, 2);
+};
+
+Sprite.prototype.colisaoLargura = function (ctx) {
+  if(this.x < 0){
+    this.x = 0;
+    this.vx = 0;
+    this.ax =0;
+  }
+
+  if(this.x > 385){
+    this.x = 385;
+    this.vx = 0;
+    this.ax =0;
+  }
+};
+
+Sprite.prototype.colisaoAltura = function (ctx) {
+  if(this.y < 0){
+    this.y = 0;
+    this.vy = 0;
+    this.ay = 0;
+    this.vx = 0;
+    this.ax = 0;
+
+  }
+};
+
+Sprite.prototype.pouso = function (ctx) {
+  if(naveAzul.y >= 410){
+    this.y = 410;
+    this.ax = 0;
+    this.vx = 0;
+    this.ay = 0; 
+    if(naveAzul.vy <= 55){
+      ctx.fillText("Sucesso!", 180, 250);
+    }
+    else {
+      ctx.fillText("Você falhou!", 180, 250);
+    }
+  }
+};
+
+
